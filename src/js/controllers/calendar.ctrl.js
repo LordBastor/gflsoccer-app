1319bff5ada85e7b738fc46cd15bdc0d21e5a094
Boris Gaganelov <b.gaganelov@avalith.bg>
2016-01-29 18:06:38 +0200

    image change

--- src/js/controllers/calendar.ctrl.js
+++ src/js/controllers/calendar.ctrl.js
@@ -1,4 +1,4 @@
-gfl.controllers.controller('CalendarCtrl', function($scope, $stateParams, Calendar)
+gfl.controllers.controller('CalendarCtrl', function($scope, $stateParams, $cordovaCalendar, Calendar)
 {
 	$scope.page_title = 'Event Calendar';
 	$scope.events = Calendar.get(function(data){});
@@ -12,9 +12,26 @@ gfl.controllers.controller('CalendarCtrl', function($scope, $stateParams, Calend
 	{
 		var url='';
 		
-		if(ionic.Platform.isIOS()){ url="http://maps.apple.com/maps?q="+encodeURIComponent(location); }
-		else{ url="http://maps.google.com?q="+encodeURIComponent(location); }
+		if(ionic.Platform.isIOS()){ url='http://maps.apple.com/maps?q='+encodeURIComponent(location); }
+		else{ url='geo://0,0?q='+encodeURIComponent(location); }
 		
 		window.open(url, '_system', 'location=no');
+		return false;
+	};
+	
+	$scope.add_event = function(gfl_event)
+	{
+		$cordovaCalendar.createEventInteractively({
+			title: gfl_event.summary,
+			location: gfl_event.location,
+			notes: gfl_event.summary,
+			// Check for dateTime or date
+			startDate: gfl_event.start.date,
+			endDate: gfl_event.end.date
+		}).then(function(result){
+			// kurami
+		}, function(err){
+			// kurami
+		});
 	};
 });
